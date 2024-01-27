ad44c6da5217ce3d5e1ef436716ed519d925cf29
ZiHao Guo <13130258983@163.com>
2024-01-27 20:35:06 +0800

    change all requests body type to byte

--- request.go
+++ request.go
@@ -19,7 +19,7 @@ func NewSession() *HttpSession {
 }
 
 // Get Get请求
-func (h *HttpSession) Get(url_ string, headers map[string]string) (*Response, error) {
+func (h *HttpSession) Get(url_ string, headers Headers) (*Response, error) {
 	parsedUrl, err := url.Parse(url_)
 	if err != nil {
 		return nil, err
@@ -41,7 +41,7 @@ func (h *HttpSession) Get(url_ string, headers map[string]string) (*Response, er
 }
 
 // Post Post请求
-func (h *HttpSession) Post(url_ string, headers map[string]string, body string) (*Response, error) {
+func (h *HttpSession) Post(url_ string, headers Headers, body []byte) (*Response, error) {
 	parsedUrl, err := url.Parse(url_)
 	if err != nil {
 		return nil, err
@@ -55,7 +55,7 @@ func (h *HttpSession) Post(url_ string, headers map[string]string, body string) 
 		Method: http.MethodPost,
 		URL:    parsedUrl,
 		Header: header,
-		Body:   io.NopCloser(bytes.NewReader([]byte(body))),
+		Body:   io.NopCloser(bytes.NewReader(body)),
 	}
 	resp, err := h.Client.Do(request)
 	if err != nil {
@@ -65,7 +65,7 @@ func (h *HttpSession) Post(url_ string, headers map[string]string, body string) 
 }
 
 // PostByteBody Post请求， with byte body
-func (h *HttpSession) PostByteBody(url_ string, headers map[string]string, body []byte) (*Response, error) {
+func (h *HttpSession) PostByteBody(url_ string, headers Headers, body []byte) (*Response, error) {
 	parsedUrl, err := url.Parse(url_)
 	if err != nil {
 		return nil, err
@@ -89,7 +89,7 @@ func (h *HttpSession) PostByteBody(url_ string, headers map[string]string, body 
 }
 
 // PostForm PostForm请求
-func (h *HttpSession) PostForm(url_ string, headers map[string]string, body url.Values) (*Response, error) {
+func (h *HttpSession) PostForm(url_ string, headers Headers, body url.Values) (*Response, error) {
 	parsedUrl, err := url.Parse(url_)
 	if err != nil {
 		return nil, err
@@ -113,7 +113,7 @@ func (h *HttpSession) PostForm(url_ string, headers map[string]string, body url.
 }
 
 // PostJson PostJson请求
-func (h *HttpSession) PostJson(url_ string, headers map[string]string, body JsonT) (*Response, error) {
+func (h *HttpSession) PostJson(url_ string, headers Headers, body any) (*Response, error) {
 	parsedUrl, err := url.Parse(url_)
 	if err != nil {
 		return nil, err
@@ -141,7 +141,7 @@ func (h *HttpSession) PostJson(url_ string, headers map[string]string, body Json
 }
 
 // Put Put请求
-func (h *HttpSession) Put(url_ string, headers map[string]string, body string) (*Response, error) {
+func (h *HttpSession) Put(url_ string, headers Headers, body []byte) (*Response, error) {
 	parsedUrl, err := url.Parse(url_)
 	if err != nil {
 		return nil, err
@@ -154,7 +154,7 @@ func (h *HttpSession) Put(url_ string, headers map[string]string, body string) (
 		Method: http.MethodPut,
 		URL:    parsedUrl,
 		Header: header,
-		Body:   io.NopCloser(bytes.NewReader([]byte(body))),
+		Body:   io.NopCloser(bytes.NewReader(body)),
 	}
 	resp, err := h.Client.Do(request)
 	if err != nil {
@@ -164,7 +164,7 @@ func (h *HttpSession) Put(url_ string, headers map[string]string, body string) (
 }
 
 // Delete Delete请求
-func (h *HttpSession) Delete(url_ string, headers map[string]string) (*Response, error) {
+func (h *HttpSession) Delete(url_ string, headers Headers) (*Response, error) {
 	parsedUrl, err := url.Parse(url_)
 	if err != nil {
 		return nil, err
@@ -186,7 +186,7 @@ func (h *HttpSession) Delete(url_ string, headers map[string]string) (*Response,
 }
 
 // Options Options请求
-func (h *HttpSession) Options(url_ string, headers map[string]string) (*Response, error) {
+func (h *HttpSession) Options(url_ string, headers Headers) (*Response, error) {
 	parsedUrl, err := url.Parse(url_)
 	if err != nil {
 		return nil, err
